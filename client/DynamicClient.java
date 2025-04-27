cce1855252841cb48b25b54f1425188226ed0041
الراشي مؤدم <moedem.errachi@etudiant-fst.utm.tn>
2025-04-27 22:03:32 +0100

    amelioration

--- client/DynamicClient.java
+++ client/DynamicClient.java
@@ -1,34 +1,28 @@
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.rmi.server.RMIClassLoader;
 import java.util.Properties;
 
 public class DynamicClient {
     
-    public DynamicClient () throws Exception {
+    public DynamicClient (String[] args) throws Exception{
         Properties p = System.getProperties();
         String url = p.getProperty("java.rmi.server.codebase");
 
-        Class<?> ClasseClient = RMIClassLoader.loadClass(url, "GameClientGUI");
+        Class<?> ClasseClient = RMIClassLoader.loadClass(url , "GameClientGUI");
         System.out.println("Class loaded: " + ClasseClient.getName());
+        Constructor<?> [] C = ClasseClient.getConstructors();
         
-        Constructor<?>[] constructors = ClasseClient.getConstructors();
-        
-        // Create an instance
-        Object guiInstance = constructors[0].newInstance(new Object[]{});
-        
-        // Make sure it's visible
-        Method setVisibleMethod = ClasseClient.getMethod("setVisible", boolean.class);
-        setVisibleMethod.invoke(guiInstance, true);
+        C[0].newInstance(new Object[]{args});
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String [] args) {
         System.setSecurityManager(new SecurityManager());
         try {
-            new DynamicClient();
-        } catch (Exception e) {
+            new DynamicClient(args);
+        }catch (Exception e){
             System.out.println(e.toString());
-            e.printStackTrace(); // To help debug
         }
+        
     }
 }
