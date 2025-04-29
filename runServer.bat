30f4f8366656cc08d71fb08f10b9b7215767fcc9
abderrazek-houidi <abderrazek.houidi@etudiant-fst.utm.tn>
2025-04-29 08:57:56 +0100

    fixed dynamic Run

--- runServer.bat
+++ runServer.bat
@@ -1,2 +1,2 @@
 cd server
-java "-Djava.security.policy=server.policy" "-Djava.rmi.server.codebase=file:///C:xampp/htdocs/TicTacToe/" GameServer
+java "-Djava.security.policy=server.policy" "-Djava.rmi.server.codebase=http://localhost/TicTacToe/" GameServer
